0b0dc774702670b707aac899effe4340c1dcc6cf
Sachin <sachinnpatil25@gmail.com>
2025-09-04 13:08:47 +0530

    Added fibonacci program

---
+++ src/Program/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package Program;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=0;
+		int b=1;
+		int sum =0;
+		int i=1;
+		while(i<10)
+		{
+			sum=a+b;
+			System.out.println(sum);
+			a=b;
+			b=sum;
+			i++;
+		}
+	}
+
+}
